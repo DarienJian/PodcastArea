07c46cf84bc9a3045e367b272dc6a45738b11616
Darien <r880514@gmail.com>
2021-09-25 15:12:21 +0800

    init

---
+++ podcast-area/podcast-area/podcast_areaApp.swift
@@ -0,0 +1,17 @@
+//
+//  podcast_areaApp.swift
+//  podcast-area
+//
+//  Created by Akira Akimoto on 2021/9/25.
+//
+
+import SwiftUI
+
+@main
+struct podcast_areaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
